b0832b9941f53eebd2416004f3e44ae927e396ed
manulangat <emmanuelthedeveloper@gmail.com>
2020-09-22 15:45:25 +0300

    ft:adding_colors_config_on_the_console_app_listen:backnd

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT
 
 app.use('/',(req,res) => res.send('Hey'))
 
-app.listen(PORT, console.log(`Portfolio running on ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Portfolio running on ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.underline))
 
 
 
