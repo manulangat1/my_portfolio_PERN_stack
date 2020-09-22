8fa06a319c525001c13f64bc0520fead608b565c
manulangat <emmanuelthedeveloper@gmail.com>
2020-09-22 15:37:26 +0300

    ft:setup_the_basic_env:backend

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,22 @@
 import express from 'express'
-import express from 'express'
-import express from 'express'
-import express from 'express'
+import colors from 'colors'
+import dotenv from 'dotenv'
+import path from 'path'
+import morgan from 'morgan'
+
+dotenv.config()
+const app = express()
+
+
+app.use(express.json())
+if (process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+const PORT = process.env.PORT 
+
+app.use('/',(req,res) => res.send('Hey'))
+
+app.listen(PORT, console.log(`Portfolio running on ${process.env.NODE_ENV} mode on port ${PORT}`))
+
+
+
