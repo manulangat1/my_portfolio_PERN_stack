d85987ad0fe9760cc274a40ecf104c8705b341ee
manulangat <emmanuelthedeveloper@gmail.com>
2020-09-27 16:15:13 +0300

    ft-installing-and-creating-models-backend

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,17 @@ import colors from 'colors'
 import dotenv from 'dotenv'
 import path from 'path'
 import morgan from 'morgan'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
 
 dotenv.config()
 const app = express()
 
-
+app.use(cors())
 app.use(express.json())
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 if (process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
