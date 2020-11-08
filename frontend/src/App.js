8852c901836f2286a6c4fbd34e0cc22380278bb6
manulangat <emmanuelthedeveloper@gmail.com>
2020-11-08 14:18:13 +0300

    ft-adding-config-syles

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+
+import './styles/main.scss';
+import React from 'react';
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+
+
+class App extends React.Component{
+  render(){
+    return(
+      <section>
+      <Header />
+      <h1>hdgdgd</h1>
+      <Footer />
+    </section>
+    )
+  }
+}
+export default App;
